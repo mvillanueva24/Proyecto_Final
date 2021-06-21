3f71c4a7497e2294810f2e6e67adfe4e5e9efdb3
mvillanueva24 <mariorvillanueva24@gmail.com>
2021-06-21 18:19:42 +0000

    Avance

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,31 +7,27 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card-group">
+            <div class="card-deck">
                 <div class="card">
-                    <!--<img class="card-img-top" src="img/bolsa.PNG" alt="Card image cap">-->
+                  <img class="card-img-top" src="..." alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
                     
-                    <div class="card-body">
-                    <h5 class="card-title">Realizar Venta</h5>
-                    </div>
+                  </div>
                 </div>
                 <div class="card">
+                  <img class="card-img-top" src="..." alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
                     
-                    <div class="card-body">
-                    <h5 class="card-title">Inventario</h5>
-                    </div>
+                  </div>
                 </div>
                 <div class="card">
+                  <img class="card-img-top" src="..." alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
                     
-                    <div class="card-body">
-                    <h5 class="card-title">Clientes</h5>
-                    </div>
-                </div>
-                <div class="card">
-                    
-                    <div class="card-body">
-                    <h5 class="card-title">Usuarios</h5>
-                    </div>
+                  </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,32 +4,32 @@
             col-12 col-md-3 col-xl-2 bd-sidebar collapse
             "col-12 col-md-3 col-xl-1 ml-4 d-md-block bd-sidebar collapse"
         -->
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
+        <ul class="navbar-nav mr-auto h5">
+            <li class="nav-item mb-1">
                 <a class="nav-link active" href="#">
                     <span data-feather="home"></span>
                     Inicio <span class="sr-only">(current)</span>
                   </a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item mb-1">
                 <a class="nav-link" href="#">
                   <span data-feather="file"></span>
                   Vender
                 </a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item mb-1">
                 <a class="nav-link" href="/admin/productos">
                   <span data-feather="shopping-cart"></span>
                   Productos
                 </a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item mb-1">
                 <a class="nav-link" href="/admin/clientes">
                   <span data-feather="users"></span>
                   Clientes
                 </a>
               </li>
-              <li class="nav-item dropdown">
+              <li class="nav-item dropdown mb-1">
                 <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Caja</a>
                 <div class="dropdown-menu" aria-labelledby="dropdown01">
                   <a class="dropdown-item" href="#">Ventas al Contado</a>
@@ -37,7 +37,7 @@
                   <a class="dropdown-item" href="#">Caja</a>
                 </div>
               </li>
-              <li class="nav-item">
+              <li class="nav-item mb-1">
                 <a class="nav-link" href="#">
                   <span data-feather="layers"></span>
                   Usuarios
