79abd76fab3ae0d42187b7affd53c068afd0c835
mvillanueva24 <mariorvillanueva24@gmail.com>
2021-06-26 07:34:58 +0000

    Archivos Seeder

--- database/seeders/SaleCollectionSeeder.php
+++ database/seeders/SaleCollectionSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class SaleSeeder extends Seeder
+class SaleCollectionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
